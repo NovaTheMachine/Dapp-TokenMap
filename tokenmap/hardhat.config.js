423549f2d5d21628a97fa60bf98f4a4bfb00d3e7
NovaTheMachine <nfdistri@gmail.com>
2021-11-03 07:14:28 +0100

    config and nft sart contract

---
+++ tokenmap/hardhat.config.js
@@ -0,0 +1,29 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString()
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.infura.io/v3/d5b5a5f91fae4fd1b5f9eb66fc3e2295",
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: "https://polygon-mainnet.infura.io/v3/d5b5a5f91fae4fd1b5f9eb66fc3e2295",
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
