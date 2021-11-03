2bf77e1ee21401f9b99ada3b28c7de9aed7b1339
NovaTheMachine <nfdistri@gmail.com>
2021-11-03 16:13:45 +0100

    nftmarket contrat

--- tokenmap/contracts/NFTMarket.sol
+++ tokenmap/contracts/NFTMarket.sol
@@ -1,3 +1,56 @@
-// contracts/NFT.sol
+// contracts/NFTMarket.sol
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct Land {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+        uint256 CenterCoords;
+        uint256[] PolygonCoords;
+    }
+
+    mapping(uint256 => Land) private idToMarketItem;
+
+    event LandCreated(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold,
+        uint256 CenterCoords,
+        uint256[] PolygonCoords
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem(address nftContract, uint256 tokenId)
+        public
+        payable
+        nonReentrant
+    {}
+}
