f6b479451b6eca13559c7f7d8d1cf8769113a3fa
NovaTheMachine <nfdistri@gmail.com>
2021-11-06 13:42:42 +0100

    create land css

---
+++ tokenmap/pages/create-land.js
@@ -0,0 +1,168 @@
+/* pages/create-item.js */
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+import Web3Modal from 'web3modal'
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+import {
+    nftaddress, nftmarketaddress
+} from '../config'
+
+import NFT from '/home/toshiba/projects/Blockchain/HACKATHON/Dapp-TokenMap/tokenmap/artifacts/contracts/NFT.sol/NFT.json'
+import Market from '/home/toshiba/projects/Blockchain/HACKATHON/Dapp-TokenMap/tokenmap/artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+
+export default function CreateLand() {
+    const [fileUrl, setFileUrl] = useState(null)
+    const [formInput, updateFormInput] = useState({ GeolocationCoordinates: '', name: '', description: '' })
+    const router = useRouter()
+
+    async function onChange(e) {
+        const file = e.target.files[0]
+        try {
+            const added = await client.add(
+                file,
+                {
+                    progress: (prog) => console.log(`received: ${prog}`)
+                }
+            )
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            setFileUrl(url)
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }
+    }
+    async function createMarket() {
+        const { name, description, GeolocationCoordinates } = formInput
+        if (!name || !description || !GeolocationCoordinates || !fileUrl) return
+        /* first, upload to IPFS */
+        const data = JSON.stringify({
+            name, description, image: fileUrl
+        })
+        try {
+            const added = await client.add(data)
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`
+            /* after file is uploaded to IPFS, pass the URL to save it on Polygon */
+            createSale(url)
+        } catch (error) {
+            console.log('Error uploading file: ', error)
+        }
+    }
+
+    async function create(url) {
+        const web3Modal = new Web3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const signer = provider.getSigner()
+
+        /* next, create the item */
+        let contract = new ethers.Contract(nftaddress, NFT.abi, signer)
+        let transaction = await contract.createToken(url)
+        let tx = await transaction.wait()
+        let event = tx.events[0]
+        let value = event.args[2]
+        let tokenId = value.toNumber()
+
+        /* then list the item for sale on the marketplace */
+        contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+
+
+        transaction = await contract.createMarketItem(nftaddress, tokenId)
+        await transaction.wait()
+        router.push('/')
+    }
+
+    return (
+        <div className="flex justify-evenly">
+            <div className="w-1/2 flex flex-col ">
+                <input
+                    placeholder="Name"
+                    className="mt-8 border rounded p-4"
+                    onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+                />
+                <textarea
+                    placeholder="Description"
+                    className="mt-2 border rounded p-4"
+                    onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+                />
+
+
+                <div class="grid grid-cols-2 gap-2">
+                    <input
+                        placeholder="Center Coordinate lat"
+                        className="mt-2 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, centerCoordinatesLat: e.target.value })}
+                    />
+                    <input
+                        placeholder="Center Coordinate long"
+                        className="mt-2 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, centerCoordinatesLong: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                    <input
+                        placeholder="Coordinates"
+                        className="mt-1 border rounded p-4"
+                        onChange={e => updateFormInput({ ...formInput, coordinates: e.target.value })}
+                    />
+                </div>
+
+
+
+
+
+                <input
+                    type="file"
+                    name="Asset"
+                    className="my-4"
+                    onChange={onChange}
+                />
+                {
+                    fileUrl && (
+                        <img className="rounded mt-4" width="350" src={fileUrl} />
+                    )
+                }
+                <button onClick={createMarket} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+                    Create Digital Asset
+                </button>
+            </div>
+        </div>
+    )
+}
