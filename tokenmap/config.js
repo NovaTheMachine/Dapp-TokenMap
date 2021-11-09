a321a1c96897450d7ff22db8b53930dd39e14f95
NovaTheMachine <nfdistri@gmail.com>
2021-11-09 23:45:59 +0100

    commit

--- tokenmap/config.js
+++ tokenmap/config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
-  export const nftaddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+  export const nftmarketaddress = "0x393ADD1b3330f554B864Db3265b6f947d824B3BD"
+  export const nftaddress = "0x3E4D6D73Bbe3Cf5D96c30f6A8BF805087B406b04"
   
